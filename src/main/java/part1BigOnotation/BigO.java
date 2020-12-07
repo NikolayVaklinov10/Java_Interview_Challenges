8d197afe4babf1af9c363b49212c1bcf4a222bb4
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-12-07 20:45:11 +0000

    the Big O options given by order of efficiency

---
+++ src/main/java/part1BigOnotation/BigO.java
@@ -0,0 +1,17 @@
+package part1BigOnotation;
+
+public class BigO {
+
+    /**
+     *  Big-O
+     *
+     *  O(1)       Constant
+     *  O(logn)    Logarithmic
+     *  O(n)       Linear
+     *  O(nlogn)   n log-star n
+     *  O(n2)      Quadratic
+     */
+
+
+
+}
